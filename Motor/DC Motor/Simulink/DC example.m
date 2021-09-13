9d7c6ca7f0cd4641b362449d6b6d2f3ecc6768da
JongSoo Lee <dldnxks12@naver.com>
2021-09-13 18:23:22 +0900

    Update DC example.m

--- Motor/DC Motor/Simulink/DC example.m
+++ Motor/DC Motor/Simulink/DC example.m
@@ -1,24 +1,39 @@
+%% Parameter in Maxon DCX35L Motor Spec 
+
 clc; % commmand line clear
 clear all; % Workspace Variable clear
 close all; % plot clear
 
-Va = 1; % input Voltage
+%% Parameter in Maxon DCX35L Motor Spec 
 
-%% Parameter
+% input Voltage
+Va = 5; 
+
 % Circuit Transfer function
-La = 1; 
-Ra = 1;
+La = 0.000658; % 0.658 mH = 0.000658 H
+Ra = 1.76; %
 
 % Mechanical Transfer function
-J = 1;
-B = 1;
+
+% 돌림 힘  : 힘 x 거리 [N x m] = [ (kg * m / s^2  )x m] = [kg * m^2 / s^2]
+% 각가속도 : [1 rad / s^2]
+% 돌림 힘 = 회전 관성 모멘트 x 각가속도 (I * alpha)
+% 회전 관성 모멘트 I = 돌림 힘 / 각가속도 
+% 따라서 단위는 [ kg * m^2 ] !
+
+%1kg = 1000g
+%1m = 100cm
+%99.5 x 0.001 x 0.01 x 0.01
+J = 0.00000995; 
+B = 0.002647 ; % J/time constant : 0.00000995 / 0.00376 
 
 % torque constance and emf constant
-Kt = 0.1;
-Ke = 0.1;
+% torque constant : Nm / A
+Kt = 0.0683;
+Ke = 0.0683;
 
 % Load torque
-TL = 1;
+TL = 0;
 
 %% Execute Simulink
 
@@ -37,8 +52,8 @@ x2 = ans.anguler_vel.time;
 y2 = ans.anguler_vel.signals.values;
 
 figure(1)
-plot(x, y, 'r') % x : time , y : vale, color = red
+plot(x, y, 'r') % x : time , y : vaule, color = red
 
 figure(2)
-plot(x2, y2, 'r') % x : time , y : vale, color = red
+plot(x2, y2, 'r') % x : time , y : vaule, color = red
 
