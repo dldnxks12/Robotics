e9cc1717185db8119f51f52615a96f0e51d42144
JS <dldnxks12@naver.com>
2022-01-25 16:18:49 +0900

    Env

---
+++ # Robotics - Reinforcement Learning/# Week 4/Test.py
@@ -0,0 +1,23 @@
+import numpy as np
+import gym
+import random
+from time import sleep
+import matplotlib.pyplot as plt
+
+
+env = gym.make('BipedalWalker-v3')
+env.reset()
+
+print("Action Space {}".format(env.action_space))
+print("State Space {}".format(env.observation_space))
+
+
+done = False
+for i in range(300):
+    env.render()
+    action = env.action_space.sample()
+    next_state, reward, done, _ = env.step(action)
+
+
+
+env.close()
